31b3611349dbc6804117180acc25841f1eeba38c
Fullbust505 <117484278+Fullbust505@users.noreply.github.com>
2024-01-02 19:24:19 +0100

    updated multi player mode
    got some issues, have to correct them

--- main.c
+++ main.c
@@ -1,32 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int multi_mode(){
-    return 0;
+    int win;
+    int nb_matches = 15 ;
+    while (nb_matches > 0){
+        // Player 1's turn
+        int p1_nb = -1 ;
+        while ((p1_nb != 1) || ((p1_nb != 2) || (p1_nb != 3))){
+            printf("P1 : enter a number of matches to take (1, 2 or 3) : ");
+            scanf("%d", &p1_nb);
+        }
+        nb_matches = nb_matches - p1_nb;
+        if (nb_matches <= 0) {
+            win = 2;
+            break;
+        } 
+
+        int p2_nb = -1;
+        while ((p2_nb != 1) || ((p2_nb != 2) || (p2_nb != 3))){
+            printf("P2 : enter a number of matches to take (1, 2 or 3) : ");
+            scanf("%d", &p2_nb);
+        }
+        nb_matches = nb_matches - p2_nb;
+        if (nb_matches <= 0){
+            win = 1;
+            break;
+        }
+
+    } 
+    return win;
 }
 
 int bot_mode(){
     return 0;
 }
 
-int main_menu(){
-    int replay = "Y";
-    while (replay == "Y"){
+int main(){
+
+    srand(time(NULL));   // Resets the random seed
+
+    int replay = 1;
+    while (replay == 1){
     
-        int choice;
+        int choice = 1;
         printf("Hi ! Welcome to my second mini-game ! (we're getting there) \n");
         printf("If you wish to play in 2 player mode, enter 1. Else, enter 2 to challenge a bot.\n");
         printf("If you desire to quit, enter any other number\n");
-        scanf("Please enter a number : %d", &choice);
+        printf("Please enter a number : ");
+        scanf("%d", &choice);
 
         if (choice == 1){
-            multi_mode();
+            int winner = multi_mode();
+            printf("The winner is Player %d ! Congrats !", winner);
         } else if (choice == 2){
             bot_mode();
-
-        printf("Do you desire to play again ? (Y = yes / N = no)\n");
-        scanf("Please enter your choice : %d", &replay);
-    
+            printf("Not done yet, please wait ;)");
+        }
+        printf("Do you desire to play again ? (1 = yes / 0 = no)\n");
+        printf("Please enter your choice : ");
+        scanf("%d", &replay);
     }
     return 0;
 }
